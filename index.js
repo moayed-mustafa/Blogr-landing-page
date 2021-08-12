9b4d68ac6cacea685c3c11322fb6d09915afbdb2
moayed-mustafa <moayed.abrida@yahoo.com>
2021-08-12 20:47:29 +0400

    mobile styles done, missing the hamburger menu for now

---
+++ index.js
@@ -0,0 +1,6 @@
+let openMenu = document.getElementsByClassName('nav-icon-open')
+    console.log(openMenu)
+    // openMenu.addEventListener('click', () => {
+    //     console.log("opening this thing up")
+    // })
+
